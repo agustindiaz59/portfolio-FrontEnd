732e45f39a5ac838da0a12352299b95ccdd8154a
agustindiaz59 <matutias10000@gmail.com>
2023-02-06 14:56:41 -0300

    tercer commit, generando componentes de angular

--- FrontEnd-Dinamico/src/app/app.module.ts
+++ FrontEnd-Dinamico/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienciasComponent } from './components/experiencias/experiencias.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    EncabezadoComponent,
+    AboutComponent,
+    ExperienciasComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+Repositorio FrontEnd para argentina programa
+
 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
